cf11f1009003bd2a41ef6571e601d744b55fcbf2
Tran-Trung-Nhut <ttnhut904@gmail.com>
2025-01-01 23:47:48 +0700

    change DBURL

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,13 +1,19 @@
-import { Pool } from "pg";
-import {drizzle} from "drizzle-orm/node-postgres"
+// import { Pool } from "pg";
+// import {drizzle} from "drizzle-orm/node-postgres"
+
+import {drizzle} from "drizzle-orm/vercel-postgres"
 import dotenv from 'dotenv'
+import { sql } from "@vercel/postgres";
+import * as schema from "./schema"
 
 dotenv.config()
 
-const pool = new Pool({
+// Dùng cho Node hoặc local
+/* const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {rejectUnauthorized: false}
 })
+export const db = drizzle(pool) */
 
-
-export const db = drizzle(pool)
+// Dùng cho vercel
+export const db = drizzle(sql, { schema })
